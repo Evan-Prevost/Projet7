25de4f7b0becfc0b59d820efed62b17f039c9427
Evan-Prevost <115547264+Evan-Prevost@users.noreply.github.com>
2023-01-24 18:05:57 +0100

    noice

--- Projet7/Program.cs
+++ Projet7/Program.cs
@@ -3,32 +3,50 @@
 using System;
 
 
+
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Choose a save :\n");
+        string state0 = " ► Continue\n   New Game";
+        string state1 = "   Continue\n ► New Game";
+        int state = 0;
+        //Console.WriteLine("Choose a save :\n");
         if (!File.Exists("save"))
         {
             //File.Create("save");
-            Console.WriteLine(" ► New Game");
+            //Console.WriteLine(" ► New Game");
+            Console.WriteLine(state0);
         }
         else
         {
             Console.WriteLine(" ► Continue\n    New Game");
         }
-        while (Console.ReadKey().Key != ConsoleKey.Enter) {
+        bool loop = true;
+        //while(Console.ReadKey().Key != ConsoleKey.Entre){}
+        while (loop) {
             ConsoleKey e = Console.ReadKey().Key;
-            switch(e)
+            switch (e)
             {
-
-                case ConsoleKey.UpArrow:
-                    Console.WriteLine("a");
+                case ConsoleKey.Enter:
+                    loop = false;
+                    break;
+                case ConsoleKey.UpArrow or ConsoleKey.DownArrow:
+                    Console.Clear();
+                    if (state == 0)
+                    {
+                        Console.WriteLine(state1);
+                        state = 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine(state0);
+                        state = 0;
+                    }
                     break;
                 default:
-                    Console.WriteLine("a");
                     break;
-            }        
+            }
         }
 
     }
